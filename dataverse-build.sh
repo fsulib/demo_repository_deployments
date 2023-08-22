e7e0d3f1e7f1f7b3ed328be09fcdf29f75380dce
Bryan J. Brown <bjbrown@fsu.edu>
2023-08-22 09:21:57 -0500

    Adds superuser toggle to build

--- dataverse-build.sh
+++ dataverse-build.sh
@@ -2,3 +2,7 @@ cd /
 git clone https://github.com/IQSS/dataverse-docker
 cd /dataverse-docker
 bash ./demostart.sh
+
+echo "Waiting for Dataverse to wake up..."
+sleep 300
+docker exec dataverse bash -c 'curl -X POST "http://localhost:8080/api/admin/superuser/dataverseAdmin"'
