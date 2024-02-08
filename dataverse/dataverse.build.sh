e81f162bd77150d42fd580c75fc2862f4b677b30
Bryan J. Brown <bjbrown@fsu.edu>
2024-02-08 10:51:28 -0600

    Final adjustment to minimize total scripts

--- dataverse/dataverse.build.sh
+++ dataverse/dataverse.build.sh
@@ -1,8 +1,17 @@
-cd /
-git clone https://github.com/IQSS/dataverse-docker
-cd /dataverse-docker
-bash ./demostart.sh
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime
 
-echo "Waiting for Dataverse to wake up..."
+apt update
+apt -y upgrade
+apt -y install zip unzip apt-transport-https
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt -y install docker-ce docker-compose
+
+git clone https://github.com/IQSS/dataverse-docker /dataverse-docker
+cd /dataverse-docker; ./demostart.sh
+
 sleep 300
 docker exec dataverse bash -c 'curl -X POST "http://localhost:8080/api/admin/superuser/dataverseAdmin"'
