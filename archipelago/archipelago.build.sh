a598ca2e0ffbc540d5c3496532255dfa24c72aff
Bryan J. Brown <bjbrown@fsu.edu>
2024-02-13 11:59:35 -0600

    Archipelago Vagrant builds

--- archipelago/archipelago.build.sh
+++ archipelago/archipelago.build.sh
@@ -13,9 +13,18 @@ apt -y install docker-ce docker-compose
 cd /; git clone --branch 1.3.0 https://github.com/esmero/archipelago-deployment
 cd /archipelago-deployment; git checkout 1.3.0
 cp /demo_repository_deployments/archipelago/archipelago.docker-compose.yml /archipelago-deployment/docker-compose.yml
-#docker-compose pull
-#docker-compose up -d
-#chown -R 8183:8183 persistent/iiifcache
-#chown -R 8983:8983 persistent/solrcore
-#docker exec -ti esmero-php bash -c "chown -R www-data:www-data private"
-#docker exec -ti esmero-minio bash -c "cd /root; curl https://dl.min.io/client/mc/release/linux-amd64/mc --output mc; chmod +x mc; mv mc /usr/local/bin/"
+docker-compose pull
+docker-compose up -d
+chown -R 8183:8183 persistent/iiifcache
+chown -R 8983:8983 persistent/solrcore
+docker exec esmero-php bash -c "chown -R www-data:www-data private"
+docker exec esmero-minio bash -c "cd /root; curl https://dl.min.io/client/mc/release/linux-amd64/mc --output mc; chmod +x mc; mv mc /usr/local/bin/"
+docker exec esmero-minio bash -c "cd /data; mc mb archipelago; mc anonymous set public archipelago"
+docker exec esmero-php bash -c "composer install"
+docker exec esmero-php bash -c 'scripts/archipelago/setup.sh'
+docker exec -u www-data esmero-php bash -c "cd web;../vendor/bin/drush -y si --verbose --existing-config --db-url=mysql://root:esmerodb@esmero-db/drupal --account-name=admin --account-pass=archipelago -r=/var/www/html/web --sites-subdir=default --notify=false;drush cr;chown -R www-data:www-data sites;"
+docker exec esmero-php bash -c 'drush urol administrator "admin"'
+docker exec esmero-php bash -c 'drush user:password admin admin'
+docker exec esmero-php bash -c 'drush ucrt jsonapi --password="jsonapi"; drush urol metadata_api "jsonapi"'
+docker exec esmero-php bash -c 'drush ucrt demo --password="demo"; drush urol metadata_pro "demo"'
+docker exec esmero-php bash -c 'scripts/archipelago/deploy.sh'
