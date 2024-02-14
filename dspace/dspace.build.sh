a01198fe000131b58422bd78acb6f42536b88147
Bryan J. Brown <bjbrown@fsu.edu>
2024-02-14 09:33:19 -0600

    Maybe this will work

--- dspace/dspace.build.sh
+++ dspace/dspace.build.sh
@@ -14,6 +14,7 @@ cd /; git clone https://github.com/fsulib/demo_repository_deployments
 cd /; git clone https://github.com/DSpace/dspace-angular
 cd /dspace-angular; git checkout dspace-7_x
 cp /demo_repository_deployments/dspace/custom.cli.yml /dspace-angular/docker/cli.yml
+cp /demo_repository_deployments/dspace/dspace.env /dspace-angular/.env
 docker-compose -f docker/docker-compose.yml -f docker/docker-compose-rest.yml pull
 docker-compose -p d7 -f docker/docker-compose.yml -f docker/docker-compose-rest.yml -f docker/db.entities.yml up -d
 docker network create docker_dspacenet
